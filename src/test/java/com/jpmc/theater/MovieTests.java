e78fe223c994c3d77519aa48a793f3122676e584
Lamoreina <elaine.sf.burchman@gmail.com>
2022-09-17 18:47:40 -0400

    additional unit tests for new ticket pricing rules

--- src/test/java/com/jpmc/theater/MovieTests.java
+++ src/test/java/com/jpmc/theater/MovieTests.java
@@ -2,16 +2,14 @@ package com.jpmc.theater;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MovieTests {
 
     //todo: set up default movie to use in testing to avoid redundancy
+    //todo: set up time global vars to test
     //todo: edge cases with negative values need to be handled in logic!
 
     @Test
@@ -49,5 +47,82 @@ public class MovieTests {
         assertEquals(8, spiderMan.calculateTicketPrice(showing));
     }
 
+    @Test
+    void calculateTicketPrice_secondMovieAndSpecialMovieExpensiveTicket_SpecialDiscountApplied() {
+        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),1200, 1);
+        Showing showing = new Showing(spiderMan, 2, LocalDateTime.of(LocalDate.now(), LocalTime.now()));
+        assertEquals((1200 * .8), spiderMan.calculateTicketPrice(showing));
+    }
+//------------New Rules Applied------------------------------------
+    @Test
+    void calculateTicketPrice_firstMovieAndSpecialMovieExpensiveTicketMatineeWindow_MatineeDiscountApplied() {
 
+        LocalDate todaysDate = LocalDate.now();
+        LocalTime time = LocalTime.of(11, 00);
+        LocalDateTime localDateTime = LocalDateTime.of(todaysDate, time);
+
+        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),1200, 1);
+        Showing showing = new Showing(spiderMan, 1, localDateTime);
+        assertEquals((1200 * .75), spiderMan.calculateTicketPrice(showing));
+    }
+
+    @Test
+    void calculateTicketPrice_firstMovieAndSpecialMovieInexpensiveTicketMatineeWindow_SequenceDiscountApplied() {
+
+        LocalDate todaysDate = LocalDate.now();
+        LocalTime time = LocalTime.of(11, 00);
+        LocalDateTime localDateTime = LocalDateTime.of(todaysDate, time);
+
+        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),10, 1);
+        Showing showing = new Showing(spiderMan, 1, localDateTime);
+        assertEquals(7, spiderMan.calculateTicketPrice(showing));
+    }
+
+    @Test
+    void calculateTicketPrice_EightMovieNotSpecialNotMatineeWindowSeventhDayOfMonth_DateDiscountApplied() {
+        LocalDate todaysDate = LocalDate.of(2080, Month.AUGUST, 7);
+
+        LocalTime time = LocalTime.of(10, 59);
+        LocalDateTime localDateTime = LocalDateTime.of(todaysDate, time);
+
+        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),10, 0);
+        Showing showing = new Showing(spiderMan, 8, localDateTime);
+        assertEquals(9, spiderMan.calculateTicketPrice(showing));
+    }
+
+    @Test
+    void calculateTicketPrice_EightMovieSpecialNotMatineeWindowSeventhDayOfMonth_SpecialDiscountApplied() {
+        LocalDate todaysDate = LocalDate.of(2080, Month.AUGUST, 7);
+
+        LocalTime time = LocalTime.of(10, 59);
+        LocalDateTime localDateTime = LocalDateTime.of(todaysDate, time);
+
+        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),10, 1);
+        Showing showing = new Showing(spiderMan, 8, localDateTime);
+        assertEquals(8, spiderMan.calculateTicketPrice(showing));
+    }
+
+    @Test
+    void calculateTicketPrice_EightMovieSpecialNotMatineeWindowSeventhDayOfMonthInexpensiveTicket_DateDiscountApplied() {
+        LocalDate todaysDate = LocalDate.of(2080, Month.AUGUST, 7);
+
+        LocalTime time = LocalTime.of(10, 59);
+        LocalDateTime localDateTime = LocalDateTime.of(todaysDate, time);
+
+        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),3, 1);
+        Showing showing = new Showing(spiderMan, 8, localDateTime);
+        assertEquals(2, spiderMan.calculateTicketPrice(showing));
+    }
+
+    @Test
+    void calculateTicketPrice_EightMovieNotSpecialNotMatineeWindowSixthDayOfMonth_NoDiscountApplied() {
+        LocalDate todaysDate = LocalDate.of(2080, Month.AUGUST, 6);
+
+        LocalTime time = LocalTime.of(10, 59);
+        LocalDateTime localDateTime = LocalDateTime.of(todaysDate, time);
+
+        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),10, 0);
+        Showing showing = new Showing(spiderMan, 8, localDateTime);
+        assertEquals(10, spiderMan.calculateTicketPrice(showing));
+    }
 }
